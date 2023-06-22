10ec399ced886f43c6d56c349512c106154e91ce
Mae Krisine Vannirrie Sarmiento <msarmiento_190000001023@uic.edu.ph>
2023-06-23 00:58:56 +0800

    chore: rename folder & relocate file

---
+++ src/utils/theme.js
@@ -0,0 +1,92 @@
+import avatar from '../assets/img/avatar.png';
+import avatar1 from '../assets/img/avatars/1.png';
+import avatar2 from '../assets/img/avatars/2.png';
+import avatar3 from '../assets/img/avatars/3.png';
+import avatar4 from '../assets/img/avatars/4.png';
+import avatarborder from '../assets/img/avatars/avatar-border.png';
+
+import gradpic from '../assets/img/grad-pic.jpg';
+
+import bevast from '../assets/img/mobileapps/bevast.png';
+import heremi from '../assets/img/mobileapps/heremi.png';
+import reproChatbot from '../assets/img/webapps/repro.png';
+import reproAdmin from '../assets/img/webapps/repro-admin.png';
+
+import graphic1 from '../assets/img/graphics/1.png';
+import graphic2 from '../assets/img/graphics/2.png';
+import graphic3 from '../assets/img/graphics/3.png';
+import graphic4 from '../assets/img/graphics/4.png';
+import graphic5 from '../assets/img/graphics/5.png';
+import graphic6 from '../assets/img/graphics/6.png';
+import graphic7 from '../assets/img/graphics/7.png';
+import graphic8 from '../assets/img/graphics/8.png';
+import graphic9 from '../assets/img/graphics/9.png';
+import graphic10 from '../assets/img/graphics/10.png';
+import graphic11 from '../assets/img/graphics/11.png';
+import graphic12 from '../assets/img/graphics/12.png';
+import graphic13 from '../assets/img/graphics/13.png';
+import graphic14 from '../assets/img/graphics/14.png';
+import graphic15 from '../assets/img/graphics/15.png';
+import graphic16 from '../assets/img/graphics/16.png';
+import graphic17 from '../assets/img/graphics/17.png';
+import graphic18 from '../assets/img/graphics/18.jpg';
+import graphic19 from '../assets/img/graphics/19.png';
+import graphic20 from '../assets/img/graphics/20.png';
+
+import heremi1 from '../assets/img/ui ux/heremi/1.png';
+import heremi2 from '../assets/img/ui ux/heremi/2.png';
+import heremi3 from '../assets/img/ui ux/heremi/3.png';
+import heremi4 from '../assets/img/ui ux/heremi/4.png';
+import heremi5 from '../assets/img/ui ux/heremi/5.png';
+import heremi6 from '../assets/img/ui ux/heremi/6.png';
+import heremi7 from '../assets/img/ui ux/heremi/7.png';
+
+import ool1 from '../assets/img/ui ux/ool/1.png';
+import ool2 from '../assets/img/ui ux/ool/2.png';
+import ool3 from '../assets/img/ui ux/ool/3.png';
+
+const theme = {
+    avatar,
+    avatar1,
+    avatar2,
+    avatar3,
+    avatar4,
+    avatarborder,
+    gradpic,
+    bevast,
+    heremi,
+    reproChatbot,
+    reproAdmin,
+    graphic1,
+    graphic2,
+    graphic3,
+    graphic4,
+    graphic5,
+    graphic6,
+    graphic7,
+    graphic8,
+    graphic9,
+    graphic10,
+    graphic11,
+    graphic12,
+    graphic13,
+    graphic14,
+    graphic15,
+    graphic16,
+    graphic17,
+    graphic18,
+    graphic19,
+    graphic20,
+    heremi1,
+    heremi2,
+    heremi3,
+    heremi4,
+    heremi5,
+    heremi6,
+    heremi7,
+    ool1,
+    ool2,
+    ool3
+};
+
+export default theme;
