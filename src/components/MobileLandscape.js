4873a7c8cac6f495e48566a4a706f43722ae07fe
Mae Krisine Vannirrie Sarmiento <msarmiento_190000001023@uic.edu.ph>
2023-07-04 20:51:38 +0800

    chore: remove unused import

--- src/components/MobileLandscape.js
+++ src/components/MobileLandscape.js
@@ -4,25 +4,18 @@ import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-import { isMobile, useMobileOrientation } from 'react-device-detect';
-
 const MobileLandscape = () => {
-
-    const { isLandscape } = useMobileOrientation();
-
-    if (isMobile && isLandscape) {
-        return (
-            <div className='background'>
-                <div className='intro-container flex mx-auto p-5'>
-                    <div className='intro'>
-                        <p className='title'>Whoops!</p>
-                        Sorry about that. Website isn't available in this view as of the moment. Stay tuned for improvements soon!
-                    </div>
+    
+    return (
+        <div className='background'>
+            <div className='intro-container flex mx-auto p-5'>
+                <div className='intro'>
+                    <p className='title'>Whoops!</p>
+                    Sorry about that. Website isn't available in this view as of the moment. Stay tuned for improvements soon!
                 </div>
             </div>
-        );
-    }
-
+        </div>
+    );
 }
 
 export default MobileLandscape;
