faa75bed8c41fdb0b505b8b4be53906a40b8937c
vannirriesarmiento <sarmiento.mkv@gmail.com>
2024-07-15 05:48:40 +0400

    feat: add github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: 'https://vannirriesarmiento.github.io',
+  base: '/portfolio',
 });
